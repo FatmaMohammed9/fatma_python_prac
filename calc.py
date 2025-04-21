a918fc6bb2205fb378b3a4cbb1201cf87094c291
FatmaMohammed9 <fatoomzz9911@gmail.com>
2025-04-21 10:06:37 +0400

    practice4

--- calc.py
+++ calc.py
@@ -1,3 +1,4 @@
+import math
 x= 10
 y= 5
 
@@ -6,4 +7,5 @@ print("Sub:", x-y)
 print("Mul:", x*y)
 print("Div:", x/y)
 print("pow:", x**y)
-
+print("Fac of x:", math.factorial(x))
+print("Fac of y:", math.factorial(y))
