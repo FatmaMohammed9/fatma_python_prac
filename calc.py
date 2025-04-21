aed698ec73fb69094b7e2babe6e38b9723f44446
FatmaMohammed9 <fatoomzz9911@gmail.com>
2025-04-21 12:28:42 +0400

    test

--- calc.py
+++ calc.py
@@ -2,10 +2,18 @@ import math
 x= 10
 y= 5
 
+print("Absolute of x:", abs(x))
+print("Absolute of y:", abs(y))
+
 print("Add:", x+y)
 print("Sub:", x-y)
 print("Mul:", x*y)
 print("Div:", x/y)
 print("pow:", x**y)
-print("Fac of x:", math.factorial(x))
+
 print("Fac of y:", math.factorial(y))
+
+if x >= 0:
+    print("Fac of x:", math.factorial(x))
+else:
+     print("Fac of x: Error(cannot compute factorial of negative number)")
